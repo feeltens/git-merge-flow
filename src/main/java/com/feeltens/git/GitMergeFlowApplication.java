4ec500fabf3117820325845dcd417e1b8641e3b7
feeltens <feeltens@gmail.com>
2025-10-26 10:21:29 +0800

    GitFlowService: clean log

--- src/main/java/com/feeltens/git/GitMergeFlowApplication.java
+++ src/main/java/com/feeltens/git/GitMergeFlowApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 合流管理系统 启动类
@@ -16,10 +17,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitMergeFlowApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitMergeFlowApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(GitMergeFlowApplication.class, args);
         log.info("=================================================");
         log.info("===\u001B[33m" + "   GitMergeFlowApplication start success   " + "\u001B[0m===");
         log.info("=================================================");
+        log.info("测试地址: \thttp://127.0.0.1:{}/", context.getEnvironment().getProperty("server.port"));
     }
 
 }
